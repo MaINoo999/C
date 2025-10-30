3e4527f61ab9152c5968c15597f358fe33669d7a
JM <135982856+MaINoo999@users.noreply.github.com>
2025-10-30 22:06:14 +0900

    Add files via upload

---
+++ Day2/ex003.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("*****************************\n");
+	printf("          리포트             \n");
+	printf("*****************************\n");
+	printf("                학과 : 컴퓨터\n");
+	printf("                학번 : 000000\n");
+	printf("                이름 : 장민우\n");
+}
---
+++ Day2/ex_1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("이름 : 장민우\n");
+	printf("나이 : 27살\n");
+	printf("주소 : 대전 광역시");
+}
